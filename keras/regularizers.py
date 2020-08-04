3c41d47e759fcb970bc466e214835acc26b9eade
Delarti <45951737+Delarti@users.noreply.github.com>
2020-08-04 17:47:52 +0200

    Update regularizers.py

--- keras/regularizers.py
+++ keras/regularizers.py
@@ -121,7 +121,9 @@ class Memoire_Regularizer(Regularizer):
         self.C_blue = K.variable(C_blue, dtype='float32')
 
     def __call__(self, x):
+        print(type(self.C_red))
         return K.sum(tf.diag_part(self.lambd * K.dot(K.transpose(K.square(x)), (self.C_red + self.C_green + self.C_blue)/3)))
+    
 
     def get_config(self):
         return {'lambd': float(self.lambd),
